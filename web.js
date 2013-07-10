a591a507b4da393ec28c498a6b9d2c786bdfdaf2
Sushain Pandit <sushain.pandit@gmail.com>
2013-07-10 19:34:35 +0000

    Modified web.js to read from fs

--- web.js
+++ web.js
@@ -6,10 +6,10 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
 
  fs.readFileSync('index.html', function(err, data){
-  
   if(err) throw err;
-  
   response.send(data.toString());
+ });
+
 });
 
 var port = process.env.PORT || 5000;
